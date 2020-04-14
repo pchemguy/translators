a1fd176ac0160811c457aad2d337a57ab07e8168
PChemGuy <pchemguy@mail.ru>
2020-04-14 23:45:05 +0300

    Initial commit

---
+++ a a CNTD.ru.js
@@ -0,0 +1,36 @@
+{
+	"translatorID": "68015def-0e1f-4488-bbd6-36dba1ed7731",
+	"label": "a a CNTD.ru",
+	"creator": "PChemGuy",
+	"target": "https?://docs.cntd.ru/",
+	"minVersion": "3.0",
+	"maxVersion": "",
+	"priority": 100,
+	"inRepository": true,
+	"translatorType": 4,
+	"browserSupport": "gcsibv",
+	"lastUpdated": "2020-04-14 20:44:37"
+}
+
+/*
+	***** BEGIN LICENSE BLOCK *****
+
+	Copyright В© 2020 PChemGuy
+
+	This file is part of Zotero.
+
+	Zotero is free software: you can redistribute it and/or modify
+	it under the terms of the GNU Affero General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	Zotero is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+	GNU Affero General Public License for more details.
+
+	You should have received a copy of the GNU Affero General Public License
+	along with Zotero. If not, see <http://www.gnu.org/licenses/>.
+
+	***** END LICENSE BLOCK *****
+*/
